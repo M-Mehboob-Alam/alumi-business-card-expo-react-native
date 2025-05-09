39a3d4588e995d7c3bdff721b2141aecc6a95533
Mehboob Alam <mehboobalam.dev@gmail.com>
2025-05-09 09:14:22 +0500

    display fontawesome icons

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { Image, StyleSheet, Text, View } from "react-native";
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 export default function App() {
   return (
     <View style={styles.container}>
@@ -7,6 +8,11 @@ export default function App() {
       <Image source={require('./assets/mehboob.png')} style={{ height: 100, width: 100, borderRadius: 50, marginTop: -140, borderColor: 'white', borderWidth: 5, }} />
       <Text style={{ fontSize: 30, fontWeight: 'bold' }}>Mehboob Alam</Text>
       <Text>A Freelance Developer</Text>
+      <View style={{ flexDirection: 'row', gap: 10, marginTop: 10 }}>
+        <FontAwesome6 name="linkedin-in" size={24} color="black" />
+        <FontAwesome6 name="at" size={24} color="black" />
+        <FontAwesome6 name="github" size={24} color="black" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
