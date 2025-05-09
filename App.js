95df0f45e6141dd3956869820c1e1988da4b0c15
Mehboob Alam <mehboobalam.dev@gmail.com>
2025-05-09 09:07:10 +0500

    display static images

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-
+import { Image, StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Alumi Becoming mobile application developer!</Text>
+      <Image source={require('./assets/banner.png')} style={{ aspectRatio: 16 / 14, width: '100%', resizeMode: 'contain' }} />
+      <Image source={require('./assets/mehboob.png')} style={{ height: 100, width: 100, borderRadius: 50, marginTop: -140, borderColor: 'white', borderWidth: 5, }} />
+      <Text style={{ fontSize: 30, fontWeight: 'bold' }}>Mehboob Alam</Text>
+      <Text>A Freelance Developer</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
